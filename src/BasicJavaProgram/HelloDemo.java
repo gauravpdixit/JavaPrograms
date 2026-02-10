1ca54dac64cc942a8309bf0122f193e1749218e9
gauravpdixit <gauravdixit1595@gmail.com>
2026-02-11 00:04:00 +0530

    New Added

---
+++ src/BasicJavaProgram/HelloDemo.java
@@ -0,0 +1,11 @@
+package BasicJavaProgram;
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java");
+	}
+
+}
